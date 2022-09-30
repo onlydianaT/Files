bc33316d18e0d6428906354d728f160aa60fbdb0
Diana Bayazitova <onlydi@mail.ru>
2022-09-30 15:22:19 +0500

    Created new repos

---
+++ src/Basket.java
@@ -0,0 +1,68 @@
+import java.io.*;
+
+public class Basket implements Serializable {
+    private String[] product = {"Moloko ", "Xleb ", "Grechka "};
+    private int[] prices = {50, 14, 80};
+    private int[] numbers = {1, 2, 3};
+
+    private int[] sumProducts = new int[3];
+    private int[] numberOfProduct = new int[3];
+    private int priceOfGoods = 0;
+
+
+    public Basket() {
+        this.prices = prices;
+        this.product = product;
+        this.numbers = numbers;
+
+    }
+
+    public void addToCart(int productNum, int amount) {
+        numberOfProduct[productNum] = productNum;
+        priceOfGoods += (amount * prices[productNum]);
+        sumProducts[productNum] += amount;
+    }
+
+    public void printCart() {
+        System.out.println("Vasha korzina: ");
+        for (int i = 0; i < numberOfProduct.length; i++) {
+            if (sumProducts[i] != 0) {
+                System.out.println(numbers[i] + " " + product[i] + " " + sumProducts[i] + " shtuk, " + "cena " +
+                        prices[i] + " rub/za shtuku, " + "Vsego za dannii tovar " + (prices[i] * sumProducts[i]) + " rub");
+            }
+        }
+        System.out.println("Itogovaya stoimost' " + priceOfGoods + " rub");
+    }
+
+    public Basket serialization(File file) throws IOException {
+        Basket basket = new Basket();
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file));
+        for (int i = 0; i < numberOfProduct.length; i++) {
+            if (sumProducts[i] != 0) {
+                String s = numbers[i] + " " + product[i] + " " + sumProducts[i] + " shtuk, " + "cena " +
+                        prices[i] + " rub/za shtuku, " + "Vsego za dannii tovar " + (prices[i] * sumProducts[i]) + " rub";
+                out.writeObject(s);
+            }
+        }
+        out.close();
+        return basket;
+    }
+
+    public static Basket deserialization(File file) throws IOException, ClassNotFoundException {
+        Basket basket = new Basket();
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
+        String d = (String) in.readObject();
+        try {
+            while (!d.equals(null)) {
+                System.out.println(d);
+                d = (String) in.readObject();
+            }
+        } catch (IOException e) {
+        }
+        in.close();
+        return basket;
+    }
+}
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,110 @@
+ import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    private static boolean loadEnable;
+    private static File fileName;
+    private static String fileFormat;
+
+    private static boolean saveEnable;
+    private static File saveName;
+    private static String saveFormat;
+
+    private static boolean logEnable;
+    private static File logFileName;
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, ParserConfigurationException, SAXException, XPathExpressionException {
+
+        String[] product = {"Moloko ", "Xleb ", "Grechka "};
+        int[] prices = {50, 14, 80};
+        int[] numbers = {1, 2, 3};
+
+        Basket basket = new Basket();
+        ClientLog clientLog = new ClientLog();
+        File xml = new File("shop.xml");
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(xml);
+
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        XPath xPath = xPathFactory.newXPath();
+        loadEnable = Boolean.parseBoolean(xPath.evaluate("/config/load/enabled", doc));
+        fileFormat = (xPath.evaluate("/config/load/format", doc));
+        fileName = new File((xPath.evaluate("/config/load/fileName", doc)) + "." + fileFormat);
+        saveEnable = Boolean.parseBoolean(xPath.evaluate("/config/save/enabled", doc));
+        saveFormat = (xPath.evaluate("/config/save/format", doc));
+        saveName = new File((xPath.evaluate("/config/save/fileName", doc)) + "." + saveFormat);
+        logEnable = Boolean.parseBoolean(xPath.evaluate("/config/log/enabled", doc));
+        logFileName = new File((xPath.evaluate("/config/log/fileName", doc)));
+
+        if (loadEnable) {
+            basket = basket.deserialization(fileName);
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Vi hotite izmenit' sostav korzini? Vvedite yes or no");
+        String input = scanner.nextLine();
+        if ("no".equals(input)) {
+            if (loadEnable) {
+                System.out.println("Vasha korzina:");
+                basket = basket.deserialization(fileName);
+            }
+        } else {
+            for (int i = 0; i < numbers.length; i++) {
+                System.out.println(numbers[i] + " " + product[i] + prices[i] + " rub/shtuka");
+            }
+            while (true) {
+                System.out.println("Viberete nomer tovara i kolichestvo ili napishite end");
+                String in = scanner.nextLine();
+                if ("end".equals(in)) {
+                    break;
+                } else {
+                    String[] parts = in.split(" ");
+                    int productNumber = Integer.parseInt(parts[0]) - 1;// 0=1.1=2.2=3
+                    int productCount = Integer.parseInt(parts[1]);// kolichestvo
+                    basket.addToCart(productNumber, productCount);
+                    if (logEnable) {
+                        clientLog.log(productNumber, productCount);
+                        clientLog.exportAsCSV(logFileName);
+                    }
+                    if (saveEnable) {
+                        basket.serialization(saveName);
+                    }
+                }
+            }
+            basket.printCart();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
